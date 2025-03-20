80f0ef3071c29ccee12fe8bee91ae67380e47f2f
AdrianRojasGomez <arojasgomez.93@gmail.com>
2025-03-19 22:18:52 -0300

    Actualizado .gitignore y removidos archivos ignorados

--- cmake-build-debug/CMakeFiles/fc-tmp/raylib/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing update step for raylib")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/Users/azokka/CLionProjects/goblin_RL/cmake-build-debug/CMakeFiles/fc-tmp/raylib/raylib-gitupdate.cmake")
-
-endblock()
